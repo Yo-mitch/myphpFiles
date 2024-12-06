307c8363d6d68d562b5ab853b817dd8449f20383
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-06 23:43:49 +0000

    added a file

---
+++ variables.php
@@ -0,0 +1,17 @@
+<!-- 
+variables - a reusable container that holds data
+strings, integer, float, boolean 
+
+1. variables are case sensitive
+2. shouldn't begin with a number
+3. can begin with a letter or underscore
+4. variables are represented by dollar sign foloowed by variable name -->
+<?php 
+    $name = "Andy";
+
+    $email = "mitchy99@gmail.com";
+
+    echo "{$name} is my name and my email is  {$email}";
+
+
+?>

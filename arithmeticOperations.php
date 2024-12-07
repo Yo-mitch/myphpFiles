49b2fcc6430c1f09d9be25a2ef04dd7977a33853
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2024-12-07 22:53:52 +0000

    added a file

---
+++ arithmeticOperations.php
@@ -0,0 +1,19 @@
+<?php
+    // this tool consists of basic arithmetic operators
+    // addition (+)
+    // subraction (-)
+    // multiplication (*)
+    // division (/) 
+    // power (**)
+    // modulus (%)
+    // Increment  ++
+    // Decrement --
+
+    // Operator Procedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+
+?>

278cd0aa0e7289a4a715aabd3131f1fd41c670de
Andy-Techy <adjeimitchel03@gmail.com>
2025-04-27 10:07:55 +0000

    added a file. function to be detected later

---
+++ crud/view.php
@@ -0,0 +1,13 @@
+<!-- this file displays the data collected from the index file -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> sign up successful! </h3>
+</body>
+</html>
